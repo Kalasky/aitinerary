81de7288dc9ba0d617b421b6bf41d39877a8c216
Robert Kalasky <kalaskyr@gmail.com>
2023-10-03 11:54:25 -0400

    dist

--- backend/index.js
+++ backend/index.js
@@ -4,6 +4,7 @@ const cors = require("cors");
 const { MongoClient, ServerApiVersion } = require("mongodb");
 const port = process.env.PORT || 5000;
 const uri = process.env.MONGODB_URI;
+const path = require('path'); // Changed this line
 
 // import routes
 const tripRoutes = require("./routes/tripRoutes");
@@ -12,14 +13,23 @@ const mapRoutes = require("./routes/mapRoutes");
 // Middlewares
 app.use(cors());
 app.use(express.json());
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).json({ message: "Something broke." });
-});
 
 // use routes
 app.use("/api/trip", tripRoutes);
 app.use("/api/map", mapRoutes);
+
+// Serve static files from the React app
+app.use(express.static(path.resolve(__dirname, '../frontend/dist')));
+
+// Catch-all route to handle requests for non-existent routes
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, '../frontend/dist/index.html'));
+});
+
+app.use((err, req, res, next) => {
+  console.error(err.stack);
+  res.status(500).json({ message: "Something broke." });
+});
 
 const client = new MongoClient(uri, {
   serverApi: {
